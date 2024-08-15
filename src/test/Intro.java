657a9d617b387edcd6ade1de19edb235daad83d4
putudian2013 <putudian2013@gmail.com>
2024-08-15 18:18:02 +0800

    Initial Commit

---
+++ src/test/Intro.java
@@ -0,0 +1,17 @@
+package test;
+import org.testng.annotations.Test;
+
+public class Intro {
+
+	@Test
+	public void demo() {
+		System.out.println("Hello world!!");
+	}
+	
+	@Test
+	public void secondTest() {
+		System.out.println("Good morning!!");
+	}
+	
+
+}
